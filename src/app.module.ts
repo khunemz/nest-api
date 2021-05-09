7d98865e680a27d2e5e1833b0c8e23f9725e9dd7
khunemz <khunemz@gmail.com>
2021-05-09 12:17:11 +0700

    add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [ItemModule],
+  imports: [ItemModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
